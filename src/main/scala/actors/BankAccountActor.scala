6c944039b6f2c86fe9fc4e48970f54a4dc17be09
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2022-01-14 12:52:53 +0530

    Exercise Actors

---
+++ src/main/scala/actors/BankAccountActor.scala
@@ -0,0 +1,16 @@
+package com.ab
+package actors
+
+/**
+ * Bank Account Actor
+ *    receives
+ *      -Deposit an amount
+ *      -Withdraw an amount
+ *      -Statement
+ *    replies
+ *      - Success
+ *      - Failure
+ */
+object BankAccountActor extends App {
+
+}
---
+++ src/main/scala/actors/CounterActor.scala
@@ -0,0 +1,12 @@
+package com.ab
+package actors
+
+/**
+ * Counter Actor
+ *    Increment
+ *    Decrement
+ *    Print
+ */
+object CounterActor extends App {
+
+}
